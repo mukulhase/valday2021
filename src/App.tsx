eabe4530ddf6b4498755330433068e4a4202db0b
Mukul Hase <mukulhase@gmail.com>
2021-02-15 11:40:02 +0530

    changed content

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,16 @@ function App() {
           left: "50vw",
           textAlign: "center",
           verticalAlign: "center",
-          height: "100%"
+          height: "100%",
+          width: "50vw"
         }} >
-          <div className={progress > 0/6 ? "poem" : "hidden"} >Second choice <br></br> </div>
-          <div className={progress > 1/6 ? "poem" : "hidden"} >Is never noice <br></br> </div>
-          <div className={progress > 2/6 ? "poem" : "hidden"} >But with great poise <br></br> </div>
-          <div className={progress > 3/6 ? "poem" : "hidden"} >Your babe enjoys <br></br> </div>
-          <div className={progress > 4/6 ? "poem" : "hidden"} >Cause me and the bois <br></br> </div>
-          <div className={progress > 5/6 ? "poem" : "hidden"} >Think we are really <i>toit</i> ❤️</div>
+          <div className={progress > 0/7 ? "poem" : "hidden"} >My dreamer <br></br> </div>
+          <div className={progress > 1/7 ? "poem" : "hidden"} >My companion <br></br> </div>
+          <div className={progress > 2/7 ? "poem" : "hidden"} >My lana <br></br> </div>
+          <div className={progress > 3/7 ? "poem" : "hidden"} >My chipmunk <br></br> </div>
+          <div className={progress > 4/7 ? "poem" : "hidden"} >My cuddle bud <br></br> </div>
+          <div className={progress > 5/7 ? "poem" : "hidden"} >My kitty cat <br></br> </div>
+          <div className={progress > 6/7 ? "poem" : "hidden"} >I love you ❤️</div>
         </div>
         </div>
         
